fc191bbd911d5859da9038090d74d734ab4e28c1
Eduardo <echarly02@gmail.com>
2021-08-09 23:04:03 -0300

    Inversión del texto

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@
 document.addEventListener("DOMContentLoaded", ()=>{
 
     let inp = document.querySelector("#inp");
-    let texto = document.querySelector("#texto")
+    let lbl = document.querySelector("#texto")
     const {Observable, fromEvent} = rxjs;
     const {filter, map} = rxjs.operators;
     const observable = fromEvent(inp, 'keyup');
 
     observable.subscribe((e) =>{
+        let texto = e.target.value;
+        let reverso = texto.split('').reverse().join('');
         console.log('keyup: ', e.target.value);
-        texto.innerText = e.target.value;
+        lbl.innerText = reverso;
     })
 })
 
