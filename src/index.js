444b55369661c28da3471143a9039c2052361e0e
Eduardo <echarly02@gmail.com>
2021-08-10 01:44:46 -0300

    Se incluye timer para desuscribir el observable

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 
+const buscapalabra = (palabra, oracion) =>{
+
+}
+
 document.addEventListener("DOMContentLoaded", ()=>{
 
     let inp = document.querySelector("#inp");
     let lbl = document.querySelector("#texto")
     const {Observable, fromEvent} = rxjs;
-    const {filter, map} = rxjs.operators;
+    const {mergeMap, retry} = rxjs.operators;
     const observable = fromEvent(inp, 'keyup');
 
-    observable.subscribe((e) =>{
+    let subscri = observable.subscribe((e) =>{
         let texto = e.target.value;
         let reverso = texto.split('').reverse().join('');
-        console.log('keyup: ', e.target.value);
+        if(texto.indexOf('completed') != -1){
+            console.log('Deberíamos finalizar OK');
+        }
+            console.log('keyup: ', e.target.value);
         lbl.innerText = reverso;
-    })
+    });
+
+    setTimeout(()=>{
+        subscri.unsubscribe();
+        inp.setAttribute('disabled', "");
+    }, 10000)
 })
 
